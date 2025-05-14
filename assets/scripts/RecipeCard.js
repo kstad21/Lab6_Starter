f6b1e2080d411c88c36decbbc83c31f073ae44af
kstad21 <120453937+kstad21@users.noreply.github.com>
2025-05-14 00:25:42 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -138,17 +138,17 @@ class RecipeCard extends HTMLElement {
 			<img src="${data.imgSrc}"
 			alt="${data.imgAlt}">
 			<p class="title">
-				<a href="${data.titleLnk}">"${data.titleTxt} hello"</a>
+				<a href="${data.titleLnk}">${data.titleTxt}</a>
 			</p>
-			<p class="organization">"${data.organization} hello"</p>
+			<p class="organization">${data.organization}</p>
 			<div class="rating">
-				<span>"${data.rating}"</span>
-				<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
-				<span>"(${data.numRatings})"</span>
+				<span>${data.rating}</span>
+				<img src="assets/images/icons/${data.rating}-star.svg" alt="${data.rating}" stars>
+				<span>(${data.numRatings})</span>
 			</div>
-			<time>"${data.lengthTime}"</time>
+			<time>${data.lengthTime}</time>
 			<p class="ingredients">
-				"${data.ingredients}"
+				${data.ingredients}
 			</p>
 		`;
 	}
